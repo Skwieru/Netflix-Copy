ae0bf0e6a4a7d06b70e269934012b3089cecbf85
Skwieru <mskwieru@gmail.com>
2022-05-12 07:59:53 +0200

    checking commands

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Sliders Configuration
+
 const swiper = new Swiper(".swiper", {
   loop: true,
   slidesPerView: 2,
@@ -10,4 +12,34 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+// Getting Elements
+
+const logo = document.querySelector(".logo");
+const topNav = document.querySelector(".topNavigation");
+const browseButton = document.querySelector(".browseMenu");
+
+// Functionality
+
+// Handling top menu bar
+document.addEventListener("scroll", () => {
+  let scrollPostition = window.pageYOffset;
+  console.log("works");
+  if (scrollPostition > 0) {
+    topNav.classList.add("active");
+  } else {
+    topNav.classList.remove("active");
+  }
+});
+
+// Handling logo/home button
+logo.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
+
+// handling browse menu
+
+browseButton.addEventListener("click", () => {
+  browseButton.classList.toggle("displayed");
 });
